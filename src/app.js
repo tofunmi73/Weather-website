47945cc3866e0030da324c8cb142e3f6865c9f2d
Jesutofunmi <tofunmi73@gmail.com>
2025-02-04 12:14:31 +0100

    Setup app for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for Express config
 const publicDir = path.join(__dirname, '../public')
@@ -122,6 +123,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000')
+app.listen(port, () => {
+    console.log('App is running on port ' + port)
 })
